4008983b05a71d948806412855619fffcf68fb3f
snguyen0129 <34581484+snguyen0129@users.noreply.github.com>
2020-12-13 16:56:56 -0800

    Add files via upload

---
+++ R/fire-data.R
@@ -0,0 +1,18 @@
+#' San Diego Fire Incidents data
+#'
+#' Fire and EMS calls dispatched by the San Diego Fire Communications Center.
+#'
+#' @docType data
+#'
+#' @usage data(fire)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references (2020) City of San Diego
+#'
+#' @source https://data.sandiego.gov/datasets/fire-incidents/
+#'
+#' @examples
+"fire"
